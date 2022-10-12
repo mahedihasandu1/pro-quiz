46f7de090119611cef4186072d90ca7e606c067c
Mahedi Hasan <mahedihasandu1@gmail.com>
2022-10-12 13:49:30 +0600

    cover-responsive

--- src/Component/Cover/Cover.js
+++ src/Component/Cover/Cover.js
@@ -1,12 +1,25 @@
 import React from 'react';
-import './Cover.css'
+import { Link } from 'react-router-dom';
 
+import Img from'./cover.jpg'
 const Cover = () => {
     return (
-        <div className='cover-container '>
+        <div>
+           <section className='grid sm:grid-cols1 md:grid-cols-2 gap-6 bg-orange-100'>
             <div>
-                <h2>This Is cover </h2>
+                <h1 className='text-blue-800  font-bold text-center text-5xl mt-4'>Welcome To Pro Quiz World</h1>
+                <p className='text-2xl font-bold ml-10 '>To Practice This type of Quiz Click </p>
+                <ul className='text-2xl  font-bold mt-4 ml-20'>
+                    <li  className='w-1/6 text-center font-medium mr-4 mb-5 text-white  py-2 rounded bg-primary hover:text-deep-purple-accent-400'><Link to='/quiz/1'> React</Link></li>
+                    <li className=' w-1/6 text-center font-medium mr-4 mb-5 text-white  py-2 rounded bg-primary hover:text-deep-purple-accent-400'> <Link to='/quiz/2'>Java Script</Link></li>
+                    <li className=' w-1/6 text-center font-medium mr-4 mb-5 text-white  py-2 rounded bg-primary hover:text-deep-purple-accent-400'> <Link to='/quiz/4'> Css</Link></li>
+                    <li className='w-1/6 text-center font-medium mr-4 mb-5 text-white  py-2 rounded bg-primary hover:text-deep-purple-accent-400'> <Link to='/quiz/5'>Git</Link></li>
+                </ul>
             </div>
+            <div>
+                <img src={Img} alt="" />
+            </div>
+           </section>
         </div>
     );
 };
